c7e12cd9d68548aa5cc8110897093ab9d0fc182c
Bruno Mortella <bmortella@gmail.com>
2022-05-31 18:33:03 -0300

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
         grandstander: ["Grandstander"],
       },
+      colors: {
+        primary: "#202133",
+        "secondary-green": "#318478",
+        "secondary-blue": "#3B56AA",
+        "secondary-orange": "#F69D62",
+      },
     },
   },
   plugins: [],

ed5e5445d3d01685b3743c4e972f3772dfb31758
Bruno Mortella <bmortella@gmail.com>
2022-06-02 21:07:32 -0300

    Add welcome page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Signup from "../pages/auth/Signup";
 import Login from "../pages/auth/Login";
+import Welcome from "../pages/Welcome";
 import ProtectedRoute from "../pages/auth/ProtectedRoute";
 
 import { AuthContextComponent } from "../contexts/authContext";
@@ -14,6 +15,10 @@ function App() {
         <Route path="/" element={<ProtectedRoute component={Home} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/welcome"
+          element={<ProtectedRoute component={Welcome} />}
+        />
       </Routes>
     </AuthContextComponent>
   );

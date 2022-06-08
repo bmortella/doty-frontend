10a3ca7d87d9ac559f1d0d2645a962821245431f
Bruno Mortella <bmortella@gmail.com>
2022-06-07 21:27:23 -0300

    signup conclusion done

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Signup from "../pages/auth/Signup";
 import Login from "../pages/auth/Login";
 import Welcome from "../pages/Welcome";
 import WelcomeRegisterPet from "../pages/guardian/WelcomeRegisterPet";
+import SignupConclusion from "../pages/guardian/SignupConclusion";
 import ProtectedRoute from "../pages/auth/ProtectedRoute";
 
 import { AuthContextComponent } from "../contexts/authContext";
@@ -13,22 +14,32 @@ function App() {
   return (
     <AuthContextComponent>
       <Routes>
-        <Route path="/" element={<ProtectedRoute component={Home} />} />
+        {/* COMMON ROUTES */}
+        <Route
+          path="/"
+          element={<ProtectedRoute component={Home} role="guardian" />}
+        />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+
+        {/* COMMON PROTECTED */}
         <Route
           path="/welcome"
           element={<ProtectedRoute component={Welcome} />}
         />
+
+        {/* GUARDIAN ROUTES */}
         <Route
           path="/guardian/welcome/registerPets"
-          element={<WelcomeRegisterPet />}
+          element={<ProtectedRoute component={WelcomeRegisterPet} />}
+        />
+        <Route
+          path="/guardian/welcome/signupComplete"
+          element={<ProtectedRoute component={SignupConclusion} />}
         />
       </Routes>
     </AuthContextComponent>
   );
 }
-
-// DUVIDA: COMO FAZER PARA IR PARA TAB CERTA NO SIGNUP DEPENDENDO DA ORIGEM -> usar provider
 
 export default App;

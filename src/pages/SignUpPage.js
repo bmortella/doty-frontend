0ff34604515332d9997832b79a291b09b32e0f17
TenoriousOz <lucasprofessor.se@gmail.com>
2022-06-16 22:08:57 -0300

    success form register

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -7,6 +7,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { setLocale } from "yup";
 import * as yup from "yup";
 import doty from "../assets/img/Doty.svg";
+import checkCircle from "../assets/img/check-circle.svg";
 
 function SignUpPage() {
   const authContext = useContext(AuthContext);
@@ -47,6 +48,7 @@ function SignUpPage() {
 
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [termsCheckbox, setTermsCheckbox] = useState(false);
+  const [hasSignedUp, setHasSignedUp] = useState(false);
 
   async function onSubmit(data) {
     try {
@@ -58,7 +60,7 @@ function SignUpPage() {
         "loggedInUser",
         JSON.stringify({ ...response.data })
       );
-
+      setHasSignedUp(true);
       navigate("/welcome", { replace: true }); // TODO: navegar para termos
     } catch (err) {
       // Se ha response, a API retornou uma mensagem.
@@ -78,143 +80,169 @@ function SignUpPage() {
       //console.error(err);
     }
   }
-
-  return (
-    <div className="flex flex-col lg:flex-row lg:items-center bg-secondary-blue h-screen">
-      <div className="text-primary py-8 px-4 md:flex md:justify-center lg:w-full">
-        <div className="md:w-3/5 lg:w-3/5 2xl:w-3/12 bg-white rounded-xl shadow py-8 px-10">
-          <form onSubmit={handleSubmit(onSubmit)} className="mt-6" id="signup">
-            <div className="mb-6">
-              <div className="mb-10 flex justify-center">
-                <img src={doty} alt="Logo da DOTY" />
+  if (!hasSignedUp) {
+    return (
+      <div className="flex flex-col lg:flex-row lg:items-center bg-secondary-blue h-screen 2xl:py-10">
+        <div className="text-primary py-4 px-4 md:flex md:justify-center lg:w-full">
+          <div className="md:w-3/5 lg:w-3/5 2xl:w-3/12 2xl:py-20 bg-white rounded-xl shadow py-8 px-10">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="mt-3"
+              id="signup"
+            >
+              <div className="mb-4">
+                <div className="mb-4 flex justify-center">
+                  <img src={doty} alt="Logo da DOTY" />
+                </div>
+                <label for="name" className="block mb-2 text-sm">
+                  Nome
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
+                  maxLength={32}
+                  {...register("name")}
+                />
+                {errors.name?.message && (
+                  <p className="mt-2 text-sm text-error">
+                    {errors.name.message}
+                  </p>
+                )}
               </div>
-              <label for="name" className="block mb-2 text-sm">
-                Nome
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
-                maxLength={32}
-                {...register("name")}
-              />
-              {errors.name?.message && (
-                <p className="mt-2 text-sm text-error">{errors.name.message}</p>
-              )}
+              <div className="mb-6">
+                <label for="email" className="block mb-2 text-sm">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
+                  {...register("email")}
+                />
+                {errors.email?.message && (
+                  <p className="mt-2 text-sm text-error">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+              <div className="mb-6">
+                <label for="phone" className="block mb-2 text-sm">
+                  Telefone
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
+                  maxLength={11}
+                  onKeyPress={(event) => {
+                    if (!/[0-9]/.test(event.key)) {
+                      event.preventDefault();
+                    }
+                  }}
+                  {...register("phone")}
+                />
+                {errors.phone?.message && (
+                  <p className="mt-2 text-sm text-error">
+                    {errors.phone.message}
+                  </p>
+                )}
+              </div>
+              <div className="mb-6">
+                <label for="password" className="block mb-2 text-sm">
+                  Senha
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
+                  {...register("password")}
+                />
+                {errors.password?.message && (
+                  <p className="mt-2 text-sm text-error">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+              <div className="mb-6">
+                <label for="confirmPassword" className="block mb-2 text-sm">
+                  Confirmar Senha
+                </label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
+                  {...register("confirmPassword")}
+                />
+                {errors.confirmPassword?.message && (
+                  <p className="mt-2 text-sm text-error">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex items-center mb-6">
+                <input
+                  id="terms-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-6 h-6 text-secondary-blue bg-gray-100 rounded border-gray-300 focus:ring-secondary-blue focus:ring-2"
+                  defaultChecked={termsCheckbox}
+                  onChange={() => setTermsCheckbox(!termsCheckbox)}
+                />
+                <label
+                  for="terms-checkbox"
+                  className="ml-2 text-xs font-medium"
+                >
+                  Eu sou maior de idade e concordo com os{" "}
+                  <a href="#" className="text-hyperlink-blue hover:underline">
+                    termos de uso
+                  </a>
+                </label>
+              </div>
+              <div className="mb-5">
+                <button
+                  type="submit"
+                  form="signup"
+                  className="w-full text-white bg-primary disabled:bg-neutral focus:ring-4 focus:ring-blue-300 font-normal rounded-lg text-base px-11 py-2.5 mr-2 mb-2 focus:outline-none"
+                  disabled={!termsCheckbox}
+                >
+                  Criar Conta
+                </button>
+              </div>
+            </form>
+            <div className="text-center">
+              <span>
+                Já tem uma conta?{" "}
+                <Link
+                  to="/loginpage"
+                  className="text-hyperlink-blue hover:underline"
+                >
+                  Entrar
+                </Link>
+              </span>
             </div>
-            <div className="mb-6">
-              <label for="email" className="block mb-2 text-sm">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
-                {...register("email")}
-              />
-              {errors.email?.message && (
-                <p className="mt-2 text-sm text-error">
-                  {errors.email.message}
-                </p>
-              )}
-            </div>
-            <div className="mb-6">
-              <label for="phone" className="block mb-2 text-sm">
-                Telefone
-              </label>
-              <input
-                type="text"
-                name="phone"
-                id="phone"
-                className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
-                maxLength={11}
-                onKeyPress={(event) => {
-                  if (!/[0-9]/.test(event.key)) {
-                    event.preventDefault();
-                  }
-                }}
-                {...register("phone")}
-              />
-              {errors.phone?.message && (
-                <p className="mt-2 text-sm text-error">
-                  {errors.phone.message}
-                </p>
-              )}
-            </div>
-            <div className="mb-6">
-              <label for="password" className="block mb-2 text-sm">
-                Senha
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
-                {...register("password")}
-              />
-              {errors.password?.message && (
-                <p className="mt-2 text-sm text-error">
-                  {errors.password.message}
-                </p>
-              )}
-            </div>
-            <div className="mb-6">
-              <label for="confirmPassword" className="block mb-2 text-sm">
-                Confirmar Senha
-              </label>
-              <input
-                type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-                className="text-sm text-neutral focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-0 border-b-2 border-gray-300"
-                {...register("confirmPassword")}
-              />
-              {errors.confirmPassword?.message && (
-                <p className="mt-2 text-sm text-error">
-                  {errors.confirmPassword.message}
-                </p>
-              )}
-            </div>
-            <div className="flex items-center mb-6">
-              <input
-                id="terms-checkbox"
-                type="checkbox"
-                value=""
-                className="w-6 h-6 text-secondary-blue bg-gray-100 rounded border-gray-300 focus:ring-secondary-blue focus:ring-2"
-                defaultChecked={termsCheckbox}
-                onChange={() => setTermsCheckbox(!termsCheckbox)}
-              />
-              <label for="terms-checkbox" className="ml-2 text-xs font-medium">
-                Eu sou maior de idade e concordo com os{" "}
-                <a href="#" className="text-hyperlink-blue hover:underline">
-                  termos de uso
-                </a>
-              </label>
-            </div>
-            <div className="mb-5">
-              <button
-                type="submit"
-                form="signup"
-                className="w-full text-white bg-primary disabled:bg-neutral focus:ring-4 focus:ring-blue-300 font-normal rounded-lg text-base px-11 py-2.5 mr-2 mb-2 focus:outline-none"
-                disabled={!termsCheckbox}
-              >
-                Criar Conta
-              </button>
-            </div>
-          </form>
-          <div className="text-center">
-            <span>
-              Já tem uma conta?{" "}
-              <Link
-                to="/loginpage"
-                className="text-hyperlink-blue hover:underline"
-              >
-                Entrar
-              </Link>
-            </span>
           </div>
         </div>
       </div>
+    );
+  }
+  return (
+    <div className="flex flex-row bg-secondary-blue h-screen justify-center relative">
+      <form className="bg-white rounded-lg flex flex-col items-center justify-center h-4/5 mt-12 md:w-3/5 lg:text-2xl">
+        <img src={checkCircle} alt="Simbolo de confirmação" className="mb-3" />
+        <div className="text-2xl lg:text-4xl font-bold text-[#219653] mb-3">
+          Oba!
+        </div>
+        <p className="mb-3">
+          O seu formulário foi enviado e a sua conta foi criada.
+        </p>
+        <button className="w-2/7 text-white bg-primary focus:ring-4 focus:ring-blue-300 font-normal rounded-lg text-base px-11 py-2.5 mx-2 mb-2 focus:outline-none">
+          Ir para a página inicial
+        </button>
+      </form>
     </div>
   );
 }

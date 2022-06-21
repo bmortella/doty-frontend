ce49c7cf60448d0f84778c316c9864c1ac8b3243
Ricardo Luis <ricardo-lga@live.com>
2022-06-22 00:28:22 +0100

    ladingpage

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <AuthContextComponent>
       <Routes>
-        <Route path="/" element={<ProtectedRoute component={Home} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
